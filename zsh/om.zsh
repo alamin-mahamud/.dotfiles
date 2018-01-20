d03384428311fc0f39f2094ebfbc9d2beee30176
Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-01-21 00:33:30 +0600

    updated gitignore and added symlinks scripts
    >>> Why is this Change Necessary?:
    
    >>> This change address the need by
    update: .gitignore
    add: install_symlinks.sh
    mv: install.sh -> install_symlinks_example.sh
    add: oh-my-zsh.zsh
    add: z.sh
    
    >>> What Side Effects does this change have?
    
    >>> Issue #None

---
+++ zsh/om.zsh
@@ -0,0 +1,16 @@
+export ZSH=$HOME/.oh-my-zsh
+export ZSH_THEME="random"
+
+export plugins=(
+  git
+  docker
+  django
+  pip
+  pyenv
+  pylint
+  python
+  redis-cli
+  tmux
+)
+
+source $ZSH/oh-my-zsh.sh

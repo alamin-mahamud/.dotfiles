23e339dfba7c8bea24023ed70433a4cb9cc07086
Alamin Mahamud <alamin.ineedahelp@gmail.com>
2018-02-16 13:12:57 +0600

    add: lots of cool changes
    >>> Why is this Change Necessary?:
    
    >>> This change address the need by:
    
    >>> What Side Effects does this change have?
    
    >>> Issue #None

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,12 +3,28 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   (quote
+    ("~/.emacs.d/README.org" "/home/alamin/Dropbox/org/finance.org" "/home/alamin/Dropbox/org/gcal.org" "/home/alamin/Dropbox/org/index.org" "/home/alamin/Dropbox/org/links.org" "/home/alamin/Dropbox/org/log2.org")))
  '(package-selected-packages
    (quote
-    (anaconda-mode ein flycheck helm-projectile helm-descbinds elpy py-autopep8 pyenv-mode smartparens duplicate-thing origami yasnippet undo-tree jedi guide-key which-key multiple-cursors vimish-fold helm magit htmlize org-bullets org-ac zygospore command-log-mode ace-jump-mode ace-window bm projectile expand-region company workgroups2 try ox-reveal highlight-symbol highlight-numbers pretty-mode all-the-icons-dired all-the-icons-ivy all-the-icons arjen-grey-theme f s dash use-package))))
+    (helm-gtags swiper-helm dired-details linum-relative hungry-delete beacon org-plus-contrib powerline mode-icons company-quickhelp company-jedi company-anaconda rainbow-delimiters highlight-parentheses paredit-everywhere restclient dockerfile-mode hydra telephone-line hs-minor-mode jabber paredit atomic-chrome calfw-gcal calfw org-gcal diminish exec-path-from-shell virtualenvwrapper anaconda-mode ein flycheck helm-projectile helm-descbinds elpy py-autopep8 pyenv-mode smartparens duplicate-thing origami yasnippet undo-tree jedi guide-key which-key multiple-cursors vimish-fold helm magit htmlize org-bullets org-ac zygospore command-log-mode ace-jump-mode ace-window bm projectile expand-region company workgroups2 try ox-reveal highlight-symbol highlight-numbers pretty-mode all-the-icons-dired all-the-icons-ivy all-the-icons arjen-grey-theme f s dash use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
+ '(org-block ((t (:background "#000000"))))
+ '(org-block-background ((t (:background "#000000"))))
+ '(org-block-begin-line ((t (:foreground "#008ED1" :background "#002E41"))))
+ '(org-block-end-line ((t (:foreground "#008ED1" :background "#002E41"))))
+ '(org-document-title ((t (:inherit default :weight bold :foreground "#bdc3ce" :family "Sans Serif" :height 1.5 :underline nil))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "#bdc3ce" :family "Sans Serif" :height 1.75))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "#bdc3ce" :family "Sans Serif" :height 1.5))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "#bdc3ce" :family "Sans Serif" :height 1.25))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "#bdc3ce" :family "Sans Serif" :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "#bdc3ce" :family "Sans Serif"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "#bdc3ce" :family "Sans Serif"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "#bdc3ce" :family "Sans Serif"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "#bdc3ce" :family "Sans Serif")))))

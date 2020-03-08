6e7c8a42802d58c4af44727ede49de7fb968be28
Alamin <alamin.ineedahelp@gmail.com>
2020-03-08 10:46:35 +0600

    Cleaned Slate

--- scripts/scripts/fortune.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-fort=`fortune -n 40 -s | sed ':a;N;$!ba;s/\n/ /g' | tr -s ' ' | sed "s/\"/'/g"`
-echo $fort
--- scripts/scripts/wall.sh
+++
@@ -1,5 +0,0 @@
-    #!/bin/bash
-    while true; do
-        feh --bg-fill -z ~/.config/wallpapers/
-        sleep 10
-    done

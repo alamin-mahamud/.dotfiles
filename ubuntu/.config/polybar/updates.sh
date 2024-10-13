c3f8cd31ad6f229534172fce9bf259d4022f5d4c
Alamin <alamin.ineedahelp@gmail.com>
2024-10-13 15:46:56 +0600

    Update Script includes - ubuntu support

--- ubuntu/.config/polybar/updates.sh
+++ ubuntu/.config/polybar/updates.sh
@@ -4,9 +4,10 @@ for a in $(ls /bin); do
   case $a in
     xbps-install) b=$(xbps-install -Mun | wc -l);;
     pacman) b=$(pacman -Qu | wc -l);;
+    apt) b=$(apt list --upgradable 2>/dev/null | wc -l);;
   esac
 done
 
-if [[ "$b" -ne "0" ]]; then 
+if [[ "$b" -ne "0" ]]; then
   printf "";
 fi

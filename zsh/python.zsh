915f81473804805c4a2511cd8134c51bd2d8b733
Proxy <rootproxy@duck.com>
2025-08-08 08:47:59 +0600

    Commit

--- zsh/python.zsh
+++
@@ -1,7 +0,0 @@
-export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path)"
-eval "$(pyenv init -)"
-
-export PATH="$HOME/.local/bin:$PATH"
-export PIPENV_PYTHON="$HOME/.pyenv/shims/python"
